7ac0780e14939d1628abf05a6a3ebc5a7e772dd5
Jan2134 <jan.stb8@gmail.com>
2024-01-12 15:14:23 +0100

    Create repo_first_script.py

---
+++ scripts/repo_first_script.py
@@ -0,0 +1,14 @@
+""""
+Script to make updates in github
+"""
+
+import pandas as pd
+
+def main():
+    """
+    This is the main function
+    """
+    pass
+
+if __name__ = "__main__":
+    main()
