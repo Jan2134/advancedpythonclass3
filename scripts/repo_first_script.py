3e162db883ca3e7e740e934d267e4de3bffb7695
Jan2134 <jan.stb8@gmail.com>
2024-01-12 16:08:10 +0100

    New class for filtering

--- scripts/repo_first_script.py
+++ scripts/repo_first_script.py
@@ -4,16 +4,32 @@ Script to make updates in github
 import click
 import pandas as pd
 
-click.command(short_help="Parser to import dataset")
-click.option("-f", "--filename", require=True, help="File to import")
+class FilteringClass:
+    """
+    Class for filtering
+    """
+
+    def __init__(self, df):
+        self.df = df
+
+    def filterprice(self, price):
+        """
+        filter by price
+        """
+        return self.df[self.df["Price Starting With ($)"] < price]
+
+@click.command(short_help="Parser to import dataset")
+@click.option("-f", "--filename", required=True, help="File to import")
 def main(filename):
     """
     This is the main function
     """
 
     df = pd.read_csv(filename)
+    filter_instance = FilteringClass(df)
+    result = filter_instance.filterprice(12)
+    #import pdb;pdb.set_trace()
+    print(result.shape)
 
-    print(df.shape)
-
-if __name__ = "__main__":
+if __name__ == "__main__":
     main()
