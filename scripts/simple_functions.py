885a6b3e0d430021d12821e272a7db264eac6b5c
Jan2134 <jan.stb8@gmail.com>
2024-01-15 16:19:58 +0100

    write test for functions

---
+++ scripts/simple_functions.py
@@ -0,0 +1,20 @@
+"""
+Script to write sum and substract functions
+"""
+
+def sum_two_nums(x, y):
+    """
+    Sum to numbers
+    """
+
+    return x +y
+
+def sub_two_nums(x, y):
+    """
+    Substract to numbers
+    """
+
+    return x - y 
+
+if __name__ == "__main__":
+    pass
